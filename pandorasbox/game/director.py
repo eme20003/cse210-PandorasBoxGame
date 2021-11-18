78fe303fb2e4e587302f70b189e11f74e166b507
MorganAllengit <MorganAllen@byui.edu>
2021-11-17 19:44:15 -0700

    started director and collisions

--- pandorasbox/game/director.py
+++ pandorasbox/game/director.py
@@ -1,9 +1,19 @@
+from time import sleep
+from game import constants
+
 class Director:
 
-    def __init__(self):
-        pass
+    def __init__(self, cast, script):
+        self._cast = cast
+        self._script = script
 
     def start_game(self):
-        pass
-    def _cue_action(self):
-        pass
+        while True:
+            self._cue_action('input')
+            self._cue_action('update')
+            self._cue_action('output')
+            sleep(constants.FRAME_LENGTH)
+
+    def _cue_action(self, tag):
+        for action in self._script[tag]:
+            action.execute(self._cast)
--- pandorasbox/game/handelcollisions.py
+++ pandorasbox/game/handelcollisions.py
@@ -6,7 +6,42 @@ from game.point import Point
 class HandelCollisions(Action):
 
     def execute(self, cast):
-        pass
+        balls = cast['ball'][0]
+        arrow = cast['arrow'][0]
+        pandora = cast['pandora'][0]
+
+        for ball in balls:
+            # right wall
+            if ball.get_position().equals(constants.MAX_X - 1):
+                #bounce
+                pass
+
+            # left wall
+            if ball.get_position().equals(1):
+                #bounce
+                pass
+            
+            # floor
+            if ball.get_position().equals(constants.MAX_Y - 1):
+                #bounce
+                pass
+
+            # cealing
+            if ball.get_position().equals(50):
+                #bounce
+                pass
+            
+            # pandora
+            if ball.get_position().equals(pandora.get_position()):
+                #end game
+                pass
+
+            #arrow
+            if ball.get_position().equals(arrow.get_position()):
+                #kill ball
+                #kill arrow
+                pass
+
 
     #ball hit pandora
     #arrow hit ball
