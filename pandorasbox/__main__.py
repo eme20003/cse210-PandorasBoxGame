423f6033d63d1d198947858596be903ede639102
corniab <bencornia@byui.edu>
2021-11-19 18:50:45 -0700

    basic program commit

--- pandorasbox/__main__.py
+++ pandorasbox/__main__.py
@@ -11,29 +11,110 @@ from game.moveactors import MoveActors
 from game.outputservice import OutputService
 import arcade
 
+# Constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = 'Pandoras Box'
+SCREEN_TITLE = "Pandoras Box"
 
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-arcade.set_background_color(arcade.color.GRAY)
-arcade.start_render()
+# bullet class
+class Arrow(arcade.Sprite):
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
 
-
-
-
-
-
+    def update(self):
+        """"""
 
 
+# coin class
+class Objects(arcade.Sprite):
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+
+    def update(self):
+        """"""
 
 
+# pandora class
+class Pandora(arcade.Sprite):
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+
+    def update(self):
+        """"""
 
 
+# Main Window
+class PandorasBox(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+        self.coin_list = None
+        self.bullet_list = None
+
+        # Player info
+        self.player_sprite = None
+        self.score = 0
+
+        # background color
+        arcade.set_background_color(arcade.color.GRAY)
+
+    def setup(self):
+        """Setup the window. Allows you to refresh the screen
+        instead of creating another instance."""
+
+        # Sprite Lists
+        self.player_list = arcade.SpriteList()
+        self.object_list = arcade.SpriteList()
+        self.arrow_list = arcade.SpriteList()
+
+        # add score
+        self.score = 0
+
+        # Player info
+        self.pandora = Pandora()
+        self.player_sprite.bottom = 0
+        self.player_sprite.left = 0
+
+        # add pandora to the player list
+        self.player_list.append(self.player_sprite)
+
+        # background color
+        arcade.set_background_color(arcade.color.GRAY)
+
+    def on_draw(self):
+        """THIS IS ALWAYS NEEDED. OVERRIDES default method in parent class"""
+
+        # prepare screen to draw
+        arcade.start_render()
+
+        self.coin_list.draw()
+        self.player_list.draw()
+
+        # draw the score
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 16)
+
+    def update(self, deltatime):
+        """Movement and game logic. THIS IS AUTOMATICALLY CALLED.
+        Refresh rate is 60 Hz"""
+
+        # update all the sprites
+        self.player_list.update()
+        self.object_list.update()
+        self.arrow_list.update()
+
+        # Create a collision list
+
+        # Loop through each colliding sprite, remove it, and add to the score
 
 
 arcade.finish_render()
 
 if __name__ == "__main__":
-    arcade.run()
+    window = PandorasBox(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+    # Call to setup method in PandorasBox class
+    window.setup()
+    arcade.run()
