8fe049ced4d4f49729076315b3382c9ad6ff969a
wiseman2star <wiseman2star@gmail.com>
2021-12-04 10:12:52 -0700

    better view windows, mitchell is going to add the box and the background

--- pandorasbox/game/game_over.py
+++ pandorasbox/game/game_over.py
@@ -14,15 +14,25 @@ class GameOverView(arcade.View):
     def on_draw(self):
         """Draw "Game over" across the screen."""
         arcade.start_render()
-        arcade.draw_text("Game Over", 240, 400, arcade.color.WHITE, 54)
-        arcade.draw_text("Click to restart", 310, 300, arcade.color.WHITE, 24)
+        arcade.draw_text("Game Over", SCREEN_WIDTH / 2, 400, arcade.color.WHITE, 54, anchor_x = "center")
+        # arcade.draw_text("Game Over", 240, 400, arcade.color.WHITE, 54)
+        arcade.draw_text("Clearly you missed shooting down the common cold, cancer, ", SCREEN_WIDTH / 2, 340, arcade.color.WHITE, 14,  anchor_x = "center")
+        arcade.draw_text("and the monsters under your bed and in your closet. . . . ", SCREEN_WIDTH / 2, 320, arcade.color.WHITE, 14,  anchor_x = "center")
+        arcade.draw_text("But perhaps you did some good, and restored some semblance of ", SCREEN_WIDTH / 2, 300, arcade.color.WHITE, 14, anchor_x = "center")
+        arcade.draw_text("HOPE to an otherwise doomed world.", SCREEN_WIDTH / 2, 272, arcade.color.WHITE, 19,  anchor_x = "center")
 
         time_taken_formatted = f"{round(self.time_taken, 2)} seconds"
-        arcade.draw_text(f"Time taken: {time_taken_formatted}",
+        arcade.draw_text(f"Time taken to fix what took you only a second to destroy: ",
             SCREEN_WIDTH / 2,
             200,
             arcade.color.GRAY,
             font_size = 15,
+            anchor_x = "center")
+        arcade.draw_text(time_taken_formatted,
+            SCREEN_WIDTH / 2,
+            170,
+            arcade.color.WHITE_SMOKE,
+            font_size = 22,
             anchor_x = "center")
 
         output_total = f"Total Score: {self.score}"
--- pandorasbox/game/instructions.py
+++ pandorasbox/game/instructions.py
@@ -8,8 +8,14 @@ class InstructionView(arcade.View):
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Instructions Screen", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, arcade.color.BLACK, font_size = 50, anchor_x = "center")
-        arcade.draw_text("Click to advance", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 75, arcade.color.GRAY, font_size = 20, anchor_x = "center")
+        arcade.draw_text("Instructions", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 200, arcade.color.BLACK, font_size = 40, anchor_x = "center")
+        arcade.draw_text("Pandora’s box has been opened! It is up to you to fix what you broke in the first place ", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 150, arcade.color.BLACK, font_size = 14, anchor_x = "center") 
+        arcade.draw_text("and save the world that you put in jeopardy! Shoot your arrows at plagues, monsters, hunger, ", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 130, arcade.color.BLACK, font_size = 13, anchor_x = "center")
+        arcade.draw_text("emotional stresses like when Pluto was a planet, then a mass of rocks, then a planet, then a star,", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 110, arcade.color.BLACK, font_size = 13, anchor_x = "center") 
+        arcade.draw_text("homework, and other life-threatening items as they hurtle at your face!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 90, arcade.color.BLACK, font_size = 15, anchor_x = "center")
+        arcade.draw_text("Eliminate these from existence and return the world to its state of progress-less, blissful ignorance ", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 50, arcade.color.BLACK, font_size = 13, anchor_x = "center") 
+        arcade.draw_text("where nothing good nor bad ever happens.", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 25, arcade.color.BLACK, font_size = 20, anchor_x = "center")
+        arcade.draw_text("Click to begin saving the world from your dastardly deeds.", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 100, arcade.color.GRAY, font_size = 20, anchor_x = "center")
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         # Call to setup method in PandorasBox class
