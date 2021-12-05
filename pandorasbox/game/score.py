79870a157aefea67156f602b7b1fba0fa2c1afdd
eme20003 <eme20003.byui.edu>
2021-12-04 18:12:55 -0800

    Documentation Adjustment

--- pandorasbox/game/score.py
+++ pandorasbox/game/score.py
@@ -1,9 +1,14 @@
 class Score:
     def __init__(self) -> None:
+        '''The Class contstructor
+        
+        Stereotype:
+            Information Holder'''
         self.score = 0
 
     def get_score(self):
         return self.score
 
     def add_score(self):
+        '''Adding 10 points to every enemy destroyed'''
         self.score += 10
